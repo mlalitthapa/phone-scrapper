76d93b52499a558f4b10fa1b0ae9d6e464684c7a
Lalit Thapa <mlalitthapa@gmail.com>
2019-02-14 21:39:19 +0545

    load .env file

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
 	InitializeRoute()
 }
