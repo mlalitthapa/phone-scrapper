dacad397d31bb5d06d3b177f358d0900b10d3d79
Lalit Thapa <mlalitthapa@gmail.com>
2019-02-16 10:00:08 +0545

    disable database connection

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/joho/godotenv"
-	"github.com/mlalitthapa/phone-scrapper/app"
-	"github.com/mlalitthapa/phone-scrapper/database"
 )
 
 func main() {
@@ -12,13 +10,13 @@ func main() {
 		panic("Error loading .env file")
 	}
 
-	db, err := database.Connect()
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	app.Initialize(db)
+	//db, err := database.Connect()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer db.Close()
+	//
+	//app.Initialize(db)
 
 	InitializeRoute()
 }
