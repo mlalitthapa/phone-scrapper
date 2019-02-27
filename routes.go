1dee487a59642f2e561c94d39044bbbbda583f1a
Lalit Thapa <mlalitthapa@gmail.com>
2019-02-27 21:27:45 +0545

    handle cors

--- routes.go
+++ routes.go
@@ -12,6 +12,21 @@ func InitializeRoute() {
 	r := gin.Default()
 
 	api := r.Group("/api")
+
+	api.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	v1 := api.Group("/v1")
 
 	brand.Register(v1)
